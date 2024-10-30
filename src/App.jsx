f3a738741a000e826284c0bac2f0ef4c69168fa4
Khusham Bansal <kbkhushambansal@gmail.com>
2024-10-31 03:25:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter } from 'react-router-dom';
+import Header from "./components/Header/Header.jsx"
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Header=()=>{
+    return(
+        <header>
+            <div>MOCKMATE</div>
+            <nav>
+                <div>Home</div>
+                <div>Courses</div>
+                
+
+
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
